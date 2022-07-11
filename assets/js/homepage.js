0db297d9186c90be5f7ecac6e3ba2f6842ba9971
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-10 23:20:36 -0400

    search by username for user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,66 @@
 
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
+
+
 function getUserRepos(user){
     fetch('https://api.github.com/users/' + user + '/repos')
-        .then(response => response.json())      
-        .then(data => console.log(data));
+        .then(response => {
+            if (response.ok)
+                response.json().then(data => displayRepos(data, user));
+            else
+                alert('Error: Github user not found');
+        })
+        .catch(error => alert('Unable to connect to Github'));
 }
 
 
+function displayRepos(repos, searchTerm){
+    if (!repos.length){
+        repoContainerEl.textContent = 'No repositories found';
+        return;
+    }
+    
+    repoContainerEl.textContent = '';
+    repoSearchTerm.textContent = '';
 
-// getUserRepos('noah35becker');
+    repos.forEach(repo => {
+        var repoName = repo.owner.login + '/' + repo.name;
+
+        var titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
+
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+        if (repo.open_issues_count > 0)
+            statusEl.innerHTML = '<i class="fas fa-times status-icon icon-danger"></i>' + repo.open_issues_count + ' issue(s)';
+        else
+            statusEl.innerHTML = '<i class="fas fa-check-square status-icon icon-success"></i>';
+
+        var repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+        repoEl.appendChild(titleEl);
+        repoEl.appendChild(statusEl);
+
+        repoContainerEl.appendChild(repoEl);
+    });
+}
+
+
+userFormEl.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    var username = nameInputEl.value.trim();
+
+    if (username){
+        getUserRepos(username);
+        nameInputEl.value = '';
+    }else
+        alert('Please enter a Github username');
+
+});
+
+
+
