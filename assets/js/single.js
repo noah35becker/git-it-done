052014d8ed589fc5241062b7922cd8968613a572
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-11 12:45:56 -0400

    warning when repo has > 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,13 +1,18 @@
 
 var issueContainerEl = document.querySelector('#issues-container');
-
+var limitWarningEl = document.querySelector('#limit-warning');
 
 
 function getRepoIssues(repo){ // repo parameter = 'owner/repo-name'
     fetch('https://api.github.com/repos/' + repo + '/issues?direction=asc')
         .then(response => {
             if (response.ok)
-                response.json().then(data => displayIssues(data));
+                response.json().then(data =>{
+                    displayIssues(data);
+                    
+                    if(response.headers.get('link'))
+                        displayWarning(repo);
+                });
             else
                 alert('There was a problem with your request');
         })
@@ -43,4 +48,16 @@ function displayIssues(issues){
 }
 
 
-getRepoIssues('microsoft/.net-modernization-in-a-day');
+function displayWarning(repo){
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'Github';
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+    
+    limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+    limitWarningEl.appendChild(linkEl);
+}
+
+
+getRepoIssues('angular/angular');
+// getRepoIssues('noah35becker/git-it-done');
